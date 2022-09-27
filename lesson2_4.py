7f20fd0de9e0a5810d9d861417a5c80c3f136643
DolNikita <114170412+DolNikita@users.noreply.github.com>
2022-09-27 13:20:52 +0300

    initial commit

---
+++ lesson2_4.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+import os
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    #browser.implicitly_wait(12)
+
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    button = browser.find_element(By.ID, "book")
+    button.click()
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(calc(x))
+
+    
+    # Отправляем заполненную форму
+    button = browser.find_element(By.ID, "solve")
+    button.click()
+
+    # ждем загрузки страницыы
+    time.sleep(10)
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
